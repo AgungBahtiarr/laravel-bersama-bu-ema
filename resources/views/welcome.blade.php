ab62c7e7305a330b5884ed0429d4ddeb61154952
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-11-18 05:42:15 +0700

    add tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    {{-- @vite('resources/css/app.css') --}}
+    @vite('resources/css/app.css')
 </head>
 
 <body>
-    <h1>Hello World From Server</h1>
+    <h1 class="text-4xl font-bold flex justify-center items-center h-screen">Hello World From Server🥳</h1>
+    
 </body>
 
 </html>
